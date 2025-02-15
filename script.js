25916598f9098f14122193ae63bae920fc39cc35
Santiago Guevara <dasanguepa@gmail.com>
2025-02-15 09:18:03 -0500

    Agregando funcionalidades para agregar productos

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ menu.onclick = () => {
 }
 
 /* Ocultar información del carrito de compras*/
-const btnCart = document.querySelector(".cart-container");
+const btnCart = document.querySelector(".container-cart-icon");
 
 const contenidoCarrito = document.querySelector('.cart-content');
 
@@ -20,3 +20,26 @@ btnCart.addEventListener('click', () => {
     contenidoCarrito.classList.toggle('hidden-cart')
 })
 
+/* Funcionalidades para el carrito de compra*/
+const cartInfo = document.querySelector('.cart-product') 
+const rowProduct = document.querySelector('.row-product')
+
+// Lista de todos los contenedores
+const productsList = document.querySelector('.grid-container .product')
+
+//Arreglo de todos los productos
+let allProducts = []
+
+// Verificar si se pulsa el botón de agregar al carrito
+productsList.addEventListener('click', e  => {
+    if ((e.target.classList.contains('add-product-btn'))) {
+        const product = e.target.parentElement //Almacenar el producto  seleccionado
+
+        const infoProduct = {
+            quantity: 1,
+            title: product.querySelector('h3').textContent,
+            price: product.querySelector('p').textContent,
+        }
+        console.log(infoProduct)
+    }
+})

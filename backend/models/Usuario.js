ae7c774c76e35d9ea65feb0e5d6653c92963f21b
Santiago Guevara <dasanguepa@gmail.com>
2025-03-13 16:59:40 -0500

    Configuración del modelo usuario

--- backend/models/Usuario.js
+++ backend/models/Usuario.js
@@ -1,28 +1,24 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Usuario extends Model {
     /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
+     // Asociaciones
      */
     static associate(models) {
       // Un Usuario tiene muchos pedidos
-      Usuario.hasMany(models.Pedido, {
-        foreignKey: 'usuario_id'
-      });
+      Usuario.hasMany(models.Pedido, { foreignKey: 'usuario_id'});
     }
   }
+
   Usuario.init({
     nombre: DataTypes.STRING,
     password: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'Usuario',
-    tableName: 'usuario'
+    tableName: 'usuario',
   });
   return Usuario;
-};
+}
